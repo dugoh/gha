67b837eab64d51203520d747bf5e890a8b99de48
Jacob Goense <dugo@xs4all.nl>
2026-04-24 00:04:54 +0200

    Update script.sh

--- script.sh
+++ script.sh
@@ -203,14 +203,17 @@ check download custom qemu;            wget -q -O - "${qemu_bin}"               
                                          |bunzip2 -c                                    \
                                          |tar -xf -                                     >/dev/null 2>&1 && ok || nok
 cd qemu || exit
-check install custom qemu;             sudo make install                                >/dev/null 2>&1 && ok || nok
+#check install custom qemu;             sudo make install                                >/dev/null 2>&1 && ok || nok
+sudo make install
+ls -ltr |tail -10
+cat mak*out
 cd .. || exit
 check test qemu;                       qemu --help                                      >/dev/null 2>&1 && ok || nok
 check setting qemu capabilities;       sudo setcap                                      \
                                          CAP_NET_ADMIN,CAP_NET_RAW=eip                  \
                                          /usr/local/bin/qemu                             >/dev/null 2>&1 && ok || nok
 )|format
-
+exit
 # first boot ###########################
 cat >1 <<__EOF
 (echo y; echo y)|install
