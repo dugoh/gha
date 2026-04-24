61afd560cd7abb89cc352c0e594645e4726fce1d
Jacob Goense <dugo@xs4all.nl>
2026-04-24 09:23:44 +0200

    Update script.sh

--- script.sh
+++ script.sh
@@ -208,14 +208,15 @@ check dump hardcoded symlinks;         rm ./libhw*/Makefile ./i386-softmmu/Makef
 check retarget makefile;               ln -s `pwd`/Makefile ./i386-softmmu/Makefile     >/dev/null 2>&1 && ok || nok
 check retarget hw makefiles;           ln -s `pwd`/Makefile.hw ./libhw32/Makefile \
                                          && ln -s `pwd`/Makefile.hw ./libhw64/Makefile  >/dev/null 2>&1 && ok || nok
-check install custom qemu;             sudo make install             #                   >/dev/null 2>&1 && ok || nok
+check install custom qemu;             sudo make install      2>&1 |tee /tmp/makeout       #                   >/dev/null 2>&1 && ok || nok
 cd .. || exit
 check test qemu;                       qemu --help                                      >/dev/null 2>&1 && ok || nok
 check setting qemu capabilities;       sudo setcap                                      \
                                          CAP_NET_ADMIN,CAP_NET_RAW=eip                  \
                                          /usr/local/bin/qemu                             >/dev/null 2>&1 && ok || nok
 )|format
-
+cat /tmp/makeout
+exit
 # first boot ###########################
 cat >1 <<__EOF
 (echo y; echo y)|install
